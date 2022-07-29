cf33cbd5208852d59fad8a546f9fd63eb51c825b
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-07-29 06:25:06 -0500

    commit tree ef4f4336098b16f675259d3b991d8b1b9e074b95

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :articles, only: [:index, :show]
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
